8a03f6ca9a6b6c3060c568e16a8649cdc3449800
Akira Valade <88116996+Akkibi@users.noreply.github.com>
2025-11-19 02:43:09 +0100

    add localStorage persistance for levelsDone

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -7,6 +7,8 @@ export interface LevelScoreType {
   health: number;
 }
 
+const LOCALSTORAGE_NAME = "thrust-express";
+
 type Store = {
   isEndTitle: boolean;
   setIsEndTitle: (isEndTitle: boolean) => void;
@@ -27,27 +29,51 @@ type Store = {
   lastLevelScore: LevelScoreType | null;
 };
 
+const getInitialState = () => {
+  try {
+    const persistedState = localStorage.getItem(LOCALSTORAGE_NAME);
+    if (persistedState) {
+      return JSON.parse(persistedState);
+    }
+  } catch (e) {
+    console.error("Failed to load state from localStorage", e);
+  }
+  return {
+    levelsDone: [],
+  }; // Default initial state if nothing in localStorage
+};
+
 export const useStore = create<Store>((set) => ({
+  ...getInitialState(),
   isThrusting: false,
-  setIsThrusting: (isThrusting: boolean) => set({ isThrusting }),
   isPaused: true,
   isEndTitle: false,
-  setIsEndTitle: (isEndTitle: boolean) => set({ isEndTitle }),
   isMenuOpen: true,
-  setIsMenuOpen: (isMenuOpen: boolean) => set({ isMenuOpen }),
   isLevelSelectorOpen: false,
-  setIsLevelSelectorOpen: (isLevelSelectorOpen: boolean) =>
-    set({ isLevelSelectorOpen }),
-  health: 100,
-  setHealth: (health: number) => set({ health }),
   score: 0,
-  levelsDone: [],
-  addLevelScore: (newLevelScore: LevelScoreType) =>
-    set((state) => ({
-      levelsDone: [...state.levelsDone, newLevelScore], // Create a new array
-    })),
+  health: 100,
   currentTimePassed: 0,
   thrustSpeed: 0,
   lastLevel: null,
   lastLevelScore: null,
+  setIsThrusting: (isThrusting: boolean) => set({ isThrusting }),
+  setIsEndTitle: (isEndTitle: boolean) => set({ isEndTitle }),
+  setIsMenuOpen: (isMenuOpen: boolean) => set({ isMenuOpen }),
+  setIsLevelSelectorOpen: (isLevelSelectorOpen: boolean) =>
+    set({ isLevelSelectorOpen }),
+  setHealth: (health: number) => set({ health }),
+  addLevelScore: (newLevelScore: LevelScoreType) =>
+    set((state) => ({
+      levelsDone: [...state.levelsDone, newLevelScore], // Create a new array
+    })),
 }));
+
+useStore.subscribe((state) => {
+  try {
+    const stateToSave = { levelsDone: state.levelsDone };
+
+    localStorage.setItem(LOCALSTORAGE_NAME, JSON.stringify(stateToSave));
+  } catch (e) {
+    console.error("Failed to save state to localStorage", e);
+  }
+});
